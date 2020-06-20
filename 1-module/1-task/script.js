25a620bbe555d59f65fce2de3bf9a0aa9ac6c761
Sergey Zelenov <mail@zelenov.su>
2020-06-20 14:00:58 +0200

    2nd task

--- 1-module/1-task/script.js
+++ 1-module/1-task/script.js
@@ -2,3 +2,16 @@ import Vue from '/vendor/vue.esm.browser.js';
 
 // const app = ...
 // Рекомендуется использовать МЕТОД в качестве обработчика события
+export const app = new Vue({
+  el: '#app',
+
+  data: {
+    count: 0,
+  },
+
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
+});
